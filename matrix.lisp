9803b3c3395921e145b089cc25b7e749b7e7450b
Nikita Zuev <nikitazu@gmail.com>
2015-07-10 23:38:17 +0300

    refactoring

--- matrix.lisp
+++ matrix.lisp
@@ -1,34 +1,40 @@
 ;;; Matrix
 ;;; ======
 
+(defclass matrix ()
+  (items))
+
 (defun matrix-create (size)
   (let ((matrix (make-array size))
 	(bounds (sub1 size)))
     (loop for x from 0 to bounds
 	  do (push (make-array size) matrix))
-    matrix))
+    (let ((instance (make-instance 'matrix)))
+      (setf (slot-value instance 'items)
+	    matrix)
+      instance)))
+
+(defun matrix-get (m x y)
+  (elt (elt (slot-value m 'items) x) y))
+
+(defun matrix-set (m x y v)
+  (setf (elt (elt (slot-value m 'items) x) y) v))
+
+(defun matrix-translate (x count)
+  (if (= x count)
+      0
+    (if (= x -1)
+	(sub1 count)
+      x)))
+
+;;; Utils
+;;; =====
 
 (defun iterate (f size)
   (let ((bounds (sub1 size)))
     (loop for x from 0 to bounds
 	  do (loop for y from 0 to bounds
 		   do (funcall f x y)))))
-
-(defun matrix-get (m x y)
-  (elt (elt m x) y))
-
-(defun matrix-set (m x y v)
-  (setf (elt (elt m x) y) v))
-
-(defun matrix-translate (x)
-  (if (= x *cells-count*)
-      0
-    (if (= x -1)
-	(sub1 *cells-count*)
-      x)))
-
-;;; Utils
-;;; =====
 
 (defun sub1 (n)
   (- n 1))
